9b3ff52cbd5a3e61a1d91c84a3b73b5a0969cd63
MYTH-INF <maytheinfante96@gmail.com>
2017-12-25 23:49:02 -0500

    adding files

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// cargando todo el árbol de nodos del DOM
+$(document).ready(function() {
+  // CREANDO VISTA SPLASH duración 3 segundos
+  setTimeout(function() {
+    window.location.href = 'views/home.html';
+  }, 3000);
+});
